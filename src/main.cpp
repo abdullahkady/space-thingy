696c279fddc22940c40401454e81279bc1cd4c6f
Abdullah Kady <abdullahkady@gmail.com>
2018-10-15 08:51:20 +0200

    Draw the spaceship

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,72 @@ void displayCallback(void);
 void idleCallback(void);
 //-----------------
 
+//	Global Variables
+double spaceshipX = 200, spaceshipY = 70;
+double bulletX = 0, bulletY = -100; // To be invisible by default
+//----------------
+
+void drawSpaceship()
+{
+  glPushMatrix();
+  glPointSize(10.0);
+  glLineWidth(8.0);
+
+  // FIRE BARREL THINGY?
+  glBegin(GL_LINES);
+  glColor3f(1, 0, 1);
+  glVertex2f(spaceshipX - 10, spaceshipY + 40);
+  glVertex2f(spaceshipX + 10, spaceshipY + 40);
+  glEnd();
+
+  // BODY
+  glBegin(GL_QUADS);
+  glColor3f(0.14, 0.42, 0.55);
+  glVertex2f(spaceshipX - 25, spaceshipY + 25);
+  glVertex2f(spaceshipX - 25, spaceshipY - 25);
+  glVertex2f(spaceshipX + 25, spaceshipY - 25);
+  glVertex2f(spaceshipX + 25, spaceshipY + 25);
+  glEnd();
+
+  glBegin(GL_LINES);
+  glColor3f(0, 0, 0);
+  glVertex2f(spaceshipX - 15, spaceshipY - 5);
+  glVertex2f(spaceshipX + 15, spaceshipY - 5);
+  glEnd();
+
+  // EXHAUST
+  glBegin(GL_POINTS);
+  glColor3f(1, 0, 0);
+  glVertex2f(spaceshipX - 10, spaceshipY - 30);
+  glVertex2f(spaceshipX + 10, spaceshipY - 30);
+  glEnd();
+
+  // WINGS
+  glBegin(GL_TRIANGLES);
+  glColor3f(0.2, 0.8, 0.2);
+  glVertex2f(spaceshipX + 25, spaceshipY + 25);
+  glVertex2f(spaceshipX + 25, spaceshipY - 25);
+  glVertex2f(spaceshipX + 45, spaceshipY - 35);
+  glEnd();
+
+  glBegin(GL_TRIANGLES);
+  glColor3f(0.2, 0.8, 0.2);
+  glVertex2f(spaceshipX - 25, spaceshipY + 25);
+  glVertex2f(spaceshipX - 25, spaceshipY - 25);
+  glVertex2f(spaceshipX - 45, spaceshipY - 35);
+  glEnd();
+
+  // UPPER BODY
+  glBegin(GL_QUADS);
+  glColor3f(0.99, 0.84, 0);
+  glVertex2f(spaceshipX - 25, spaceshipY + 25);
+  glVertex2f(spaceshipX - 18, spaceshipY + 40);
+  glVertex2f(spaceshipX + 18, spaceshipY + 40);
+  glVertex2f(spaceshipX + 25, spaceshipY + 25);
+  glEnd();
+  glPopMatrix();
+}
+
 void onKey(unsigned char key, int x, int y)
 {
   switch (key)
@@ -46,6 +112,7 @@ int main(int argc, char **argr)
 void displayCallback(void)
 {
   glClear(GL_COLOR_BUFFER_BIT);
+  drawSpaceship();
   glFlush();
 }
 
